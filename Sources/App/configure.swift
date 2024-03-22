57508a011ad0733d2e3d4875cc30720c684a3c8d
Gwenaël Gallon <gwenael.gallon@mac.com>
2024-03-22 01:00:36 +0100

    Update configure.swift

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -6,6 +6,19 @@ import Vapor
 
 // configures your application
 public func configure(_ app: Application) async throws {
+    // Configure custom backlog.
+    app.http.server.configuration.backlog = 128
+    // Disable address reuse.
+    app.http.server.configuration.reuseAddress = false
+    // Add 'Server: vapor' header to responses.
+    app.http.server.configuration.serverName = "proactice"
+    // Support HTTP pipelining.
+    app.http.server.configuration.supportPipelining = true
+    // Only support HTTP/2
+    app.http.server.configuration.supportVersions = [.two]
+    // Minimize packet delay.
+    app.http.server.configuration.tcpNoDelay = true
+
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
